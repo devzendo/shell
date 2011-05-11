a6168f37f0f4fccf685bc2ee263230f9da35d5f9
Matt Gumbley <matt.gumbley@devzendo.org>
2011-05-11 07:38:16 +0100

    Add the ShellPlugin and a scanner for its adapter methods.

---
+++ src/main/java/org/devzendo/shell/PluginMethodScanner.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (C) 2008-2011 Matt Gumbley, DevZendo.org <http://devzendo.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.devzendo.shell;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+public class PluginMethodScanner {
+    private static final Logger LOGGER = Logger
+            .getLogger(PluginMethodScanner.class);
+    
+    public Map<String, Method> scanPluginMethods(final ShellPlugin shellPlugin) {
+        final Map<String, Method> returnMethods = new HashMap<String, Method>();
+        final Method[] methods = shellPlugin.getClass().getMethods();
+        LOGGER.debug("Scanning " + methods.length + " method(s) from class " + shellPlugin.getClass().getSimpleName());
+        for (final Method method : methods) {
+            final String name = method.getName();
+            if (name.equals("processCommandLine") || name.equals("notify") ||
+                    name.equals("notifyAll") || name.equals("wait")) {
+                continue;
+            }
+            LOGGER.debug("Considering method " + method);
+            final Class<?> returnType = method.getReturnType();
+            final Class<?>[] parameterTypes = method.getParameterTypes();
+            if ((voidness(returnType) || iterator(returnType)) && 
+                (noParameters(parameterTypes) 
+                   || list(parameterTypes[0])
+                   || (list(parameterTypes[0]) && iterator(parameterTypes[1])))) {
+                LOGGER.debug("Registering method " + method);
+                returnMethods.put(name, method);
+            }
+        }
+        return returnMethods;
+    }
+
+    private boolean voidness(final Class<?> klass) {
+        return klass.toString().equals("void"); // no other way to detect this? 
+    }
+    private boolean iterator(final Class<?> klass) {
+        final boolean itness = klass.isAssignableFrom(Iterator.class);
+        LOGGER.debug("iteratorness of " + klass + " is " + itness);
+        return itness;
+    }
+
+    private boolean list(final Class<?> klass) {
+        return klass.isAssignableFrom(List.class);
+    }
+
+    private boolean noParameters(final Class<?>[] parameterTypes) {
+        return parameterTypes.length == 0;
+    }
+}
---
+++ src/test/java/org/devzendo/shell/TestPluginMethodScanner.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (C) 2008-2011 Matt Gumbley, DevZendo.org <http://devzendo.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.devzendo.shell;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.devzendo.commoncode.logging.LoggingUnittestHelper;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestPluginMethodScanner {
+    @BeforeClass
+    public static void setUpLogging() {
+        LoggingUnittestHelper.setupLogging();
+    }
+    
+    final PluginMethodScanner scanner = new PluginMethodScanner();
+    private abstract static class AbstractShellPlugin implements ShellPlugin {
+        public void processCommandLine(final List<String> args) {
+            // do nothing
+        }
+
+        public String getName() {
+            return "test";
+        }
+    }
+
+    private static class VoidReturnNoArgs extends AbstractShellPlugin {
+        @SuppressWarnings("unused")
+        public void funk() {
+            // do nothing
+        }
+    }
+    
+    @Test
+    public void voidReturnNoArgsOk() {
+        gotFunk(scanner.scanPluginMethods(new VoidReturnNoArgs()));
+    }
+
+    private static class VoidReturnListArgs extends AbstractShellPlugin {
+        @SuppressWarnings("unused")
+        public void funk(final List<String> args) {
+            // do nothing
+        }
+    }
+    
+    @Test
+    public void voidReturnStringArgsOk() {
+        gotFunk(scanner.scanPluginMethods(new VoidReturnListArgs()));
+    }
+
+    private static class VoidReturnListIteratorArgs extends AbstractShellPlugin {
+        @SuppressWarnings("unused")
+        public void funk(final List<String> args, final Iterator<Object> input) {
+            // do nothing
+        }
+    }
+    
+    @Test
+    public void voidReturnListIteratorArgsOk() {
+        gotFunk(scanner.scanPluginMethods(new VoidReturnListIteratorArgs()));
+    }
+
+    // TODO all the above, but with iterator return types
+
+    private void gotFunk(final Map<String, Method> map) {
+        assertThat(map.size(), equalTo(1));
+        final Iterator<String> it = map.keySet().iterator();
+        assertThat(it.hasNext(), equalTo(true));
+        assertThat(it.next(), equalTo("funk"));
+    }
+}
+
