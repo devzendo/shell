5e2f1cc61f3dbbfd4bae708d4ddc22405bf6c0cf
Matt Gumbley <matt.gumbley@devzendo.org>
2011-05-26 08:58:40 +0100

    Added the body of the RendezvousPipe.

--- src/main/java/org/devzendo/shell/pipe/RendezvousPipe.java
+++ src/main/java/org/devzendo/shell/pipe/RendezvousPipe.java
@@ -15,24 +15,65 @@
  */
 package org.devzendo.shell.pipe;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
+import org.apache.log4j.Logger;
+
 import scala.Option;
 
 public class RendezvousPipe implements InputPipe, OutputPipe {
-
+    private static final Logger LOGGER = Logger.getLogger(RendezvousPipe.class);
+    private final Object lock = new Object(); 
+    private ArrayBlockingQueue<Object> queue = new ArrayBlockingQueue<Object>(1);
+    private Thread threadCallingNext;
+    private boolean terminated = false;
+    
     @Override
     public void setTerminated() {
-        // TODO Auto-generated method stub
-        
+        LOGGER.debug("Waiting to terminate the RendezvousPipe");
+        synchronized (lock) {
+            terminated = true;
+            LOGGER.debug("Terminating the RendezvousPipe");
+            if (threadCallingNext == null) {
+                LOGGER.warn("No thread is calling next() when the RendezvousPipe is terminated");
+            } else {
+                threadCallingNext.interrupt();
+            }
+        }
     }
 
     @Override
     public void push(Object object) {
-        // TODO Auto-generated method stub
-        
+        try {
+            queue.put(object);
+        } catch (InterruptedException e) {
+            LOGGER.warn("Interrupted pushing into RendezvousPipe: " + e.getMessage());
+        }
     }
 
     @Override
     public Option<Object> next() {
-        return scala.Option.apply(null); // a.k.a. None
+        synchronized (lock) {
+            if (terminated) {
+                LOGGER.debug("Pipe terminated; not returning data");
+                return scala.Option.apply(null); // a.k.a. None
+            }
+            threadCallingNext = Thread.currentThread();
+            LOGGER.debug("Waiting for data");
+        }
+        try {
+            final Object object = queue.take();
+            LOGGER.debug("Got data " + object);
+            return new scala.Some<Object>(object);
+        } catch (InterruptedException e) {
+            // this is quite normal - see setTerminated; the message will be 
+            // 'null', so don't log it.
+            LOGGER.debug("Interrupted pulling from RendezvousPipe");
+            return scala.Option.apply(null); // a.k.a. None
+        } finally {
+            synchronized (lock) {
+                threadCallingNext = null;
+            }
+        }
     }
 }
--- src/test/java/org/devzendo/shell/pipe/TestRendezvousPipe.java
+++ src/test/java/org/devzendo/shell/pipe/TestRendezvousPipe.java
@@ -1,20 +1,99 @@
 /**
  * Copyright (C) 2008-2011 Matt Gumbley, DevZendo.org <http://devzendo.org>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.devzendo.shell.pipe;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.devzendo.commoncode.concurrency.ThreadUtils;
+import org.devzendo.commoncode.logging.LoggingUnittestHelper;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import scala.Option;
 
 public class TestRendezvousPipe {
+    private final RendezvousPipe pipe = new RendezvousPipe();
+
+    @BeforeClass
+    public static void setupLogging() {
+        LoggingUnittestHelper.setupLogging();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test(timeout = 4000)
+    public void receiverBlocksUntilObjectPushed() throws InterruptedException {
+        final CountDownLatch stored = new CountDownLatch(1);
+        final Object[] store = new Object[] {null};
+        final Thread receiver = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                store[0] = pipe.next();
+                stored.countDown();
+            }
+        });
+        receiver.start();
+        pipe.push("hello");
+        stored.await();
+        assertThat((Option<String>) store[0], OptionMatcher.isSome("hello"));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test(timeout = 4000)
+    public void receiverGetsNoneWhenTerminated() throws InterruptedException {
+        final CountDownLatch started = new CountDownLatch(1);
+        final CountDownLatch stored = new CountDownLatch(1);
+        final Object[] store = new Object[] {"replaced-with-none"};
+        final Thread receiver = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                started.countDown();
+                store[0] = pipe.next();
+                stored.countDown();
+            }
+        });
+        receiver.setName("receiver");
+        receiver.start();
+        started.await();
+        ThreadUtils.waitNoInterruption(1000); // give it chance to block on
+                                              // next()
+        pipe.setTerminated();
+        stored.await();
+        final Option<Object> option = (Option<Object>) store[0];
+        assertThat(option, OptionMatcher.isNone());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test(timeout = 4000)
+    public void receiverGetsNoneWhenTerminatedBeforeNext() throws InterruptedException {
+        pipe.setTerminated();
+        final CountDownLatch stored = new CountDownLatch(1);
+        final Object[] store = new Object[] {"replaced-with-none"};
+        final Thread receiver = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                store[0] = pipe.next();
+                stored.countDown();
+            }
+        });
+        receiver.setName("receiver");
+        receiver.start();
+        stored.await();
+        final Option<Object> option = (Option<Object>) store[0];
+        assertThat(option, OptionMatcher.isNone());
+    }
 }
