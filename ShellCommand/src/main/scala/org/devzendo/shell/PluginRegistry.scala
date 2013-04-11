e6f4fd2e15f73f6d3a5c8848e3f8b1209899c5b1
Matt Gumbley <matt.gumbley@devzendo.org>
2013-04-11 21:48:36 +0100

    Replace interface with trait.

--- ShellCommand/src/main/scala/org/devzendo/shell/PluginRegistry.scala
+++ ShellCommand/src/main/scala/org/devzendo/shell/PluginRegistry.scala
@@ -1,25 +1,24 @@
-package org.devzendo.shell;
-
-import java.util.List;
-import java.util.Set;
-
 /**
  * Copyright (C) 2008-2012 Matt Gumbley, DevZendo.org <http://devzendo.org>
- * <p/>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-public interface PluginRegistry {
-    void loadAndRegisterPluginMethods(List<ShellPlugin> staticPlugins) throws ShellPluginException;
 
-    Set<ShellPlugin> getPlugins();
+package org.devzendo.shell
+
+trait PluginRegistry {
+    @throws[ShellPluginException]
+    def loadAndRegisterPluginMethods(staticPlugins: java.util.List[ShellPlugin])
+
+    def getPlugins: java.util.Set[ShellPlugin]
 }
