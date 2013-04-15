009d14d7be4e97e7e9854fd8db54f9a47dd874d0
Matt Gumbley <matt.gumbley@devzendo.org>
2013-04-15 08:04:30 +0100

    Move Command from Java to Scala

--- ShellCommand/src/main/scala/org/devzendo/shell/Command.scala
+++ ShellCommand/src/main/scala/org/devzendo/shell/Command.scala
@@ -1,11 +1,11 @@
 /**
- * Copyright (C) 2008-2011 Matt Gumbley, DevZendo.org <http://devzendo.org>
+ * Copyright (C) 2008-2012 Matt Gumbley, DevZendo.org <http://devzendo.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,24 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.devzendo.shell;
 
-import java.util.List;
+package org.devzendo.shell
 
-public class Command {
-    private final String mName;
-    private final List<Object> mArgs;
+case class Command(
+    @scala.reflect.BeanProperty
+    name: String,
 
-    public Command(final String name, final List<Object> args) {
-        mName = name;
-        mArgs = args;
-    }
-
-    public String getName() {
-        return mName;
-    }
-
-    public List<Object> getArgs() {
-        return mArgs;
-    }
+    @scala.reflect.BeanProperty
+    args: java.util.List[AnyRef]) {
 }
