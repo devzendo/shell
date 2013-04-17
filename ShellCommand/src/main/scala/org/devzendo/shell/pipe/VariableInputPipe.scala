c0ac8909a8a454af35814c411a5ace274ba87719
Matt Gumbley <matt.gumbley@devzendo.org>
2013-04-17 18:57:04 +0100

    Move VariableInputPipe from Java to Scala

---
+++ ShellCommand/src/main/scala/org/devzendo/shell/pipe/VariableInputPipe.scala
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2008-2012 Matt Gumbley, DevZendo.org <http://devzendo.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.devzendo.shell.pipe
+
+import org.devzendo.shell.Variable
+
+class VariableInputPipe(val variable: Variable) extends InputPipe {
+    private var index = 0
+
+    def setTerminated() {
+        this.synchronized {
+            index = variable.size()
+        }
+    }
+
+    def next(): Option[AnyRef] = {
+        this.synchronized {
+            if (index < variable.size()) {
+                val prevIndex = index
+                index += 1
+                Some(variable.get(prevIndex))
+            } else {
+                None
+            }
+        }
+    }
+
+    final def getVariable: Variable = variable
+}
