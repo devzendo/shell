4202f10cad017b2cbde99207bfb2b74521a8511e
Matt Gumbley <matt.gumbley@devzendo.org>
2013-04-17 19:01:16 +0100

    Move VariableOutputPipe from Java to Scala

---
+++ ShellCommand/src/main/scala/org/devzendo/shell/pipe/VariableOutputPipe.scala
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2008-2012 Matt Gumbley, DevZendo.org <http://devzendo.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.devzendo.shell.pipe
+
+import org.devzendo.shell.Variable
+
+class VariableOutputPipe(val variable: Variable) extends OutputPipe {
+    private var terminated = false
+
+    def setTerminated() {
+        terminated = true
+    }
+
+    def push(obj: AnyRef) {
+        if (terminated) {
+            throw new IllegalStateException("Cannot push into a terminated pipe")
+        }
+        variable.add(obj)
+    }
+
+    final def getVariable: Variable = variable
+}
